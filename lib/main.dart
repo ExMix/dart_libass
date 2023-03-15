b0a7f593387386e7f650d9d80752f7efbce5e647
Yury Rakhuba <yury.rakhuba@pressfire.games>
2023-03-15 13:38:38 +0300

    first render on the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:ffi' as ffi;
 import 'dart:io';
+import 'dart:ui' as ui;
+import 'dart:math';
 
 import 'package:ffi/ffi.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as path;
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 import 'package:libass_binding/libass_binding.dart';
 
 typedef LogFn = ffi.Void Function(ffi.Int level, ffi.Pointer<ffi.Char> fmt, va_list args, ffi.Pointer<ffi.Void> ctx);
@@ -94,27 +96,32 @@ void assLog(int level, ffi.Pointer<ffi.Char> fmt, va_list args, ffi.Pointer<ffi.
   print(result);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SubtitleRenderer extends StatefulWidget {
+  const SubtitleRenderer(String id);
 
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
+  State<SubtitleRenderer> createState() => _SubtitleRendererState();
+}
+
+class ImagePainter extends CustomPainter {
+  final ui.Image image;
+
+  ImagePainter(this.image);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawImage(image, const Offset(0, 0), Paint());
+  }
+
+  @override
+  bool shouldRepaint(ImagePainter oldDelegate) {
+    return false;
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = Directory.current.path;
+class _SubtitleRendererState extends State<SubtitleRenderer> {
+  int _timeStamp = 0;
+
   late ffi.DynamicLibrary dylib;
   late LibassBindings bindings;
   ffi.Pointer<ASS_Library> libraryHandle = ffi.nullptr;
@@ -122,55 +129,114 @@ class MyAppState extends ChangeNotifier {
   ffi.Pointer<ASS_Library> libObject = ffi.nullptr;
   ffi.Pointer<ASS_Renderer> renderer = ffi.nullptr;
   ffi.Pointer<ASS_Track> track = ffi.nullptr;
-  ffi.Pointer<ASS_Image> image = ffi.nullptr;
-}
+  late ui.Image resultImage;
+  bool hasImage = false;
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var libraryPath = '';
-    if (Platform.isMacOS) {
-      var libname = 'libass.dylib';
-      libraryPath = path.join(Directory.current.path, 'lib', 'macos', libname);
-    }
-    else if (Platform.isWindows) {
-      var libname = 'ass.dll';
-      libraryPath = path.join(Directory.current.path, 'lib', 'win32', libname);
+  _SubtitleRendererState(){
+    var libname = 'ass.dll';
+    if (kDebugMode)
+    {
+      libname = 'assd.dll';
     }
 
-    var appState = context.watch<MyAppState>();
-    appState.dylib = ffi.DynamicLibrary.open(libraryPath);
-    appState.bindings = LibassBindings(appState.dylib);
-    appState.libObject = appState.bindings.ass_library_init();
+    var libraryPath = path.join(Directory.current.path, 'lib', 'win32', libname);
+    dylib = ffi.DynamicLibrary.open(libraryPath);
+    bindings = LibassBindings(dylib);
+    libObject = bindings.ass_library_init();
 
     ffi.Pointer<ffi.NativeFunction<LogFn>> logFn = ffi.Pointer.fromFunction<LogFn>(assLog);
-    appState.bindings.ass_set_message_cb(appState.libObject, logFn, ffi.nullptr);
-    appState.renderer = appState.bindings.ass_renderer_init(appState.libObject);
+    bindings.ass_set_message_cb(libObject, logFn, ffi.nullptr);
+    renderer = bindings.ass_renderer_init(libObject);
 
     String fontDirPath = path.join(Directory.current.path, 'lib', 'fonts');
     ffi.Pointer<ffi.Char> fontDirFfiPath =  fontDirPath.toNativeUtf8().cast<ffi.Char>();
-    appState.bindings.ass_set_fonts_dir(appState.libObject, fontDirFfiPath);
+    bindings.ass_set_fonts_dir(libObject, fontDirFfiPath);
 
     String defaultFont = path.join(fontDirPath, 'Montserrat-Bold.ttf');
     String defaultFamily = 'Montserrat';
-    appState.bindings.ass_set_fonts(appState.renderer, defaultFont.toNativeUtf8().cast<ffi.Char>(), defaultFamily.toNativeUtf8().cast<ffi.Char>(), 0, ffi.nullptr, 0);
-    appState.bindings.ass_set_frame_size(appState.renderer, 600, 400);
+    bindings.ass_set_fonts(renderer, defaultFont.toNativeUtf8().cast<ffi.Char>(), defaultFamily.toNativeUtf8().cast<ffi.Char>(), 1, ffi.nullptr, 0);
+    bindings.ass_set_frame_size(renderer, 600, 400);
 
     var assFilePath = path.join(Directory.current.path, 'lib', '[Erai-raws] Tomo-chan wa Onnanoko! - 01 [1080p][Multiple Subtitle][50D3873C].ANIBEL.ass');
     ffi.Pointer<ffi.Char> filePath = assFilePath.toNativeUtf8().cast<ffi.Char>();
-    appState.track = appState.bindings.ass_read_file(appState.libObject, filePath, ffi.nullptr);
+    track = bindings.ass_read_file(libObject, filePath, ffi.nullptr);
 
+    //timer = Timer(Duration(milliseconds: 16), _setTimeStamp);
+    _timeStamp = 19450 - 16;
+    _setTimeStamp();
+  }
+
+  _setTimeStamp() async {
+    _timeStamp += 16;
+    print('Timestamp $_timeStamp');
     int changeVar = 0;
     ffi.Pointer<ffi.Int> changePtr = ffi.Pointer<ffi.Int>.fromAddress(changeVar);
-    appState.image = appState.bindings.ass_render_frame(appState.renderer, appState.track, 19450, changePtr);
+    ffi.Pointer<ASS_Image> frameImage = bindings.ass_render_frame(renderer, track, _timeStamp, changePtr);
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current)
-        ],
-      ),
-    );
+    int width = frameImage.ref.w;
+    int height = frameImage.ref.h;
+
+    Uint8List memory = Uint8List(4 * width * height);
+    memory.fillRange(0, memory.length, 255);
+
+    while (frameImage != ffi.nullptr) {
+      ass_image imageRef = frameImage.ref;
+      int color = imageRef.color;
+      var r = (color >> 24) & 0xFF;
+      var g = (color >> 16) & 0xFF;
+      var b = (color >> 8) & 0xFF;
+      var a = 255 - color & 0xFF;
+
+      for (int y = 0; y < height; ++y)
+      {
+        for (int x = 0; x < width; ++x)
+        {
+          int offset = y * imageRef.stride + x;
+          double alpha = imageRef.bitmap.elementAt(offset).value / 255.0;
+          if (alpha == 0)
+          {
+            continue;
+          }
+
+          int listOffset = 4 * (y * width + x);
+          memory[listOffset] = (r * alpha).toInt();
+          memory[listOffset + 1] = (g * alpha).toInt();
+          memory[listOffset + 2] = (b * alpha).toInt();
+          memory[listOffset + 3] = (a * alpha).toInt();
+        }
+      }
+
+      frameImage = imageRef.next;
+    }
+
+    ui.decodeImageFromPixels(memory, width, height, ui.PixelFormat.rgba8888, (result)
+    {
+      _setImage(result);
+    });
+  }
+
+  _setImage(ui.Image img){
+    setState(() {
+      resultImage = img;
+      hasImage = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    if (hasImage == false) {
+      return Center(child: Text("Wait a moment", textDirection: TextDirection.ltr));
+    }
+
+    return Center(child: CustomPaint(painter: ImagePainter(resultImage)));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: SubtitleRenderer('renderer'));
   }
 }
